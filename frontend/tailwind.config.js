195f1875bfe60cf077575c5230307244b0e7435b
Kartik Tiwari <kartiktiwari@Mac.mshome.net>
2026-02-03 10:29:33 +0530

    Fix Tailwind config for Vite

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        'spin': 'spin 1s linear infinite',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
